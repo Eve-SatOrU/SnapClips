59abcf0004d978f1b6e8663922fe68cd10414a5b
lvl mja <84472102+Eve-SatOrU@users.noreply.github.com>
2023-10-29 19:20:38 +0100

    lesss gooo

---
+++ client/src/components/todolist.js
@@ -0,0 +1,63 @@
+// to do list
+todolist = [
+    {
+        id: 1,
+        title: 'home page contain section like i said in voice ',
+        completed: false
+    },
+    {
+        id: 2,
+        title: 'register page contain username and password and email ',
+        completed: false
+    },
+    {
+        id: 3,
+        title: 'login page contain username and password',
+        completed: false
+    },
+    {
+        id: 4,
+        title: 'profile page contain pic and username and email and edit password button and add information if u want also add edit button and logout button',
+        completed: false
+    },
+{
+    id :5,
+    title:'contaCT page contain email and phone number and address',
+    completed:false
+},
+{
+    id :6,
+    title:'about page contain information about the website',
+    completed:false
+},
+{
+    id:7,
+    title:'page of vedio contain input to put link and other page kifha contain download button to download vedio when ready',
+    completed:false
+},
+{
+    id:8,
+    title:'page of image contain upload image button and another one contain download button ',
+    completed:false
+},
+{
+    id:9,
+    title:'i will add some thing later ',
+    completed:false
+}
+]
+// notice don't forget vav and fotter plz
+// https://online.hitpaw.com/tiktok-watermark-remover.html?fbclid=IwAR15rYZsXMvSDPLDGgk_gSQImH5oKvn-Qm2Ry0AWPoDCgIwzxnDBIzM49LY
+// this example for website
+const shine = `
+                       _   _            _            _      _     
+                      | | | |          | |          (_)    | |    
+  __ _  ___   ___   __| | | |_   _  ___| | __   __ _ _ _ __| |___ 
+ / _\` |/ _ \ / _ \ / _\` | | | | | |/ __| |/ /  / _\` | | '__| / __|
+| (_| | (_) | (_) | (_| | | | |_| | (__|   <  | (_| | | |  | \\__ \\
+ \\__, |\\___/ \\___/ \\__,_| |_|\\__,_|\\___|_|\\_\\  \\__, |_|_|  |_|___/
+  __/ |                                         __/ |             
+ |___/                                         |___/              
+`;
+
+console.log(shine);
